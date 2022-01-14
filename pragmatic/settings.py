004e81f624b9b8589700227a26738f60e99103b6
duskite <duskite@github.com>
2022-01-14 23:03:56 +0900

    adjust Dango course 8 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -67,7 +67,8 @@ ROOT_URLCONF = 'pragmatic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 템플릿 경로 추가
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
