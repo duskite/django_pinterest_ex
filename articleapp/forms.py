13e5195fa2c57bbfbdf6806f609aebf0d0ab0069
duskite <duskite@github.com>
2022-01-18 23:59:22 +0900

    django complete

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -7,11 +7,11 @@ from projectapp.models import Project
 
 class ArticleCreationForm(ModelForm):
 
-    content = forms.CharField(widget=forms.Textarea(attrs={'class':'editable text-start',
-                                                           'style':'height: auto;'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'text-start',
+                                                           'style': 'height: auto;'}))
 
     project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
 
     class Meta:
         model = Article
-        fields = ['title','image','project','content']
+        fields = ['title', 'image', 'project', 'content']
--- projectapp/forms.py
+++ projectapp/forms.py
@@ -6,4 +6,4 @@ from projectapp.models import Project
 class ProjectCreationForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['image', 'title','description']
+        fields = ['image', 'title', 'description']
