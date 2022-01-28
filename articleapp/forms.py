99301af36b5ee209b10730b4db61f5dffaf8f62c
duskite <duskite@github.com>
2022-01-28 14:10:00 +0900

    test

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -12,7 +12,7 @@ class ArticleCreationForm(ModelForm):
     project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
 
     # 테스트 확인중인 부분
-    image = forms.ImageField(widget=forms.FileInput(attrs={'accept': 'images/*'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'accept': 'image/*'}))
 
     class Meta:
         model = Article
